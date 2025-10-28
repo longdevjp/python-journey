01cb350d5f85bab9e0c6ffc2df3b999ec7fb5482
longdevjp <turlo2k4@gmail.com>
2025-10-28 09:13:12 +0700

    Restore PythonSyntax.py

---
+++ Day1/PythonSyntax.py
@@ -0,0 +1,22 @@
+# Execute Python Syntax
+print("Hello World!")
+
+# Python Indentation 
+if 5 > 2:
+  print("Five is greater than two!")
+if 5 > 2:
+        print("Five is greater than two!") 
+
+# Python Variables
+x = 5
+y = "Hello World"
+
+# Statements
+print("Python is fun")
+
+# Many Statements
+print("Hello World!"); print("Python is fun"); print("I love coding")
+
+
+
+
