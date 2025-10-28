04590a85131dca346ce8d415a551dc2db1585453
longdevjp <turlo2k4@gmail.com>
2025-10-28 09:24:19 +0700

    Python Output

---
+++ Day2/PythonOutput.py
@@ -0,0 +1,18 @@
+# Print Text
+print("Hello, World!")
+
+# Double Quotes
+print("This is a string in double quotes.")
+print('Double Quoters')
+
+# Print Without a New Line
+print("Hello, ", end="")
+print("World!")
+
+# Print Numbers
+print(42)
+print( 3 + 5 )
+print(10 * 2)
+
+# Mix Text and Numbers
+print("Hello I'm Long", 20, 'Years Old')
