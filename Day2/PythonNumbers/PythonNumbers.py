6263980ce052ccf13e1c7b440dbc13bae1267ee1
longdevjp <turlo2k4@gmail.com>
2025-10-28 15:46:13 +0700

    Python Numbers

---
+++ Day2/PythonNumbers/PythonNumbers.py
@@ -0,0 +1,20 @@
+# Python Numbers
+# int
+# float
+# complex
+
+x = 1    # int
+y = 2.8  # float
+z = 1j   # complex
+print(type(x))
+print(type(y))
+print(type(z))
+
+# Type Conversion ((Ép kiểu))
+a = float(x)      # int to float
+b = int(y)        # float to int
+c = complex(x)    # int to complex
+
+# Random Number
+import random
+print(random.randrange(1, 10))
