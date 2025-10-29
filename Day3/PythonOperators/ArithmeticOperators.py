fb336490443cf3c0056c527a22b195e0dfc725d7
longdevjp <turlo2k4@gmail.com>
2025-10-29 21:26:26 +0700

    Python Operators P1

---
+++ Day3/PythonOperators/ArithmeticOperators.py
@@ -0,0 +1,11 @@
+# Arithmetic Operators
+x = 15
+y = 4
+print(x + y)
+print(x - y)
+print(x * y)
+print(x / y)
+print(x % y)
+print(x ** y) # mũ
+print(x // y) # chỉ lấy phần nguyên (bỏ phần thập phân)
+
---
+++ Day3/PythonOperators/AssignmentOperators.py
@@ -0,0 +1,26 @@
+"""
+=	x = 5	x = 5	
++=	x += 3	x = x + 3	
+-=	x -= 3	x = x - 3	
+*=	x *= 3	x = x * 3	
+/=	x /= 3	x = x / 3	
+%=	x %= 3	x = x % 3	
+//=	x //= 3	x = x // 3	
+**=	x **= 3	x = x ** 3	
+&=	x &= 3	x = x & 3	
+|=	x |= 3	x = x | 3	
+^=	x ^= 3	x = x ^ 3	
+>>=	x >>= 3	x = x >> 3	
+<<=	x <<= 3	x = x << 3	
+:=	print(x := 3)	x = 3
+print(x)
+"""
+
+# The Walrus Operator
+numbers = [1, 2, 3, 4, 5]
+count = len(numbers)
+if count > 3:
+    print(f"List has {count} elements")
+
+if (count := len(numbers)) > 3:
+    print(f"List has {count} elements")
