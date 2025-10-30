879ba4f90772ca427ed5ec59700f2c613e58c5b0
longdevjp <turlo2k4@gmail.com>
2025-10-30 10:14:45 +0700

    Python Lists P2

---
+++ day4/PythonLists/AccessListItems.py
@@ -0,0 +1,20 @@
+# ViDu
+thislist = ["apple", "banana", "cherry"]
+print(thislist[1]) # Output: banana
+
+# Negative Indexing
+thislist = ["apple", "banana", "cherry"]
+print(thislist[-1]) # Output: cherry
+
+# Range of Indexes
+thislist = ["apple", "banana", "cherry", "orange", "kiwi", "melon", "mango"]
+print(thislist[2:5]) # Output: ['cherry', 'orange', 'kiwi']
+
+thislist = ["apple", "banana", "cherry", "orange", "kiwi", "melon", "mango"]
+print(thislist[:4]) # Output: ['apple', 'banana', 'cherry', 'orange']
+
+thislist = ["apple", "banana", "cherry", "orange", "kiwi", "melon", "mango"]
+print(thislist[2:]) # Output: ['cherry', 'orange', 'kiwi', 'melon', 'mango']
+
+thislist = ["apple", "banana", "cherry", "orange", "kiwi", "melon", "mango"]
+print(thislist[-4:-1]) # Output: ['orange', 'kiwi', 'melon']
