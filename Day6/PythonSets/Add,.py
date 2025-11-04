af69c2ea9cf0b3508d7a47b57f4b68fc850e078e
longdevjp <turlo2k4@gmail.com>
2025-11-05 01:49:01 +0700

    Python Sets P2

---
+++ Day6/PythonSets/Add,.py
@@ -0,0 +1,16 @@
+# Add Set Items
+thisset = {"apple", "banana", "cherry"}
+thisset.add("orange")
+print(thisset)
+
+# Add Sets
+thisset = {"apple", "banana", "cherry"}
+tropical = {"pineapple", "mango", "papaya"}
+thisset.update(tropical)
+print(thisset) # Output: {'banana', 'cherry', 'apple', 'papaya', 'mango', 'pineapple'}
+
+# Add Any Iterable
+thisset = {"apple", "banana", "cherry"}
+mylist = ["kiwi", "orange"]
+thisset.update(mylist)
+print(thisset)
